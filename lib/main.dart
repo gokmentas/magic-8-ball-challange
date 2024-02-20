0b401472a6e610861e8be6024d319d5ff7f8c6b4
gokmentas <56593104+gokmentas@users.noreply.github.com>
2024-02-21 00:26:07 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp());
+  runApp(
+    const MaterialApp(
+      home: BallPage(),
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: const Text(
+          'Ask Me Anything',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Container(),
+    );
+  }
 }
